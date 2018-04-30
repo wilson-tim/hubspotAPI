59eb75d006cffecfeffd3b57ab8d88025e1e3233
Tim Wilson <tim.wilson@specialistholidays.com>
2018-04-30 10:27:26 +0100

    Remove DEBUG flag from ZLBTEST.bat

--- batch/HUBSPOT_API - ZLBTEST.bat
+++ batch/HUBSPOT_API - ZLBTEST.bat
@@ -9,22 +9,22 @@ powershell.exe %srcLocation%HUBSPOT_API_clearerrorlog.ps1
 
 echo ***********************************************************
   echo ZLBTEST LBENEWS Process Started At %TIME:~0,2%:%TIME:~3,2%  > HUBSPOT_API_%TODAY%.log
-  HubSpot_API.exe ZLBTEST ENEWS DEBUG >> HUBSPOT_API_%TODAY%.log
+  HubSpot_API.exe ZLBTEST ENEWS >> HUBSPOT_API_%TODAY%.log
 echo ***********************************************************
 
 echo ***********************************************************
   echo ZLBTEST LBBROCREQ Process Started At %TIME:~0,2%:%TIME:~3,2% >> HUBSPOT_API_%TODAY%.log
-  HubSpot_API.exe ZLBTEST BROCREQ DEBUG >> HUBSPOT_API_%TODAY%.log
+  HubSpot_API.exe ZLBTEST BROCREQ >> HUBSPOT_API_%TODAY%.log
 echo ***********************************************************
 
 echo ***********************************************************
   echo ZLBTEST LBQUOTES Process Started At %TIME:~0,2%:%TIME:~3,2% >> HUBSPOT_API_%TODAY%.log
-  HubSpot_API.exe ZLBTEST QUOTES DEBUG >> HUBSPOT_API_%TODAY%.log
+  HubSpot_API.exe ZLBTEST QUOTES >> HUBSPOT_API_%TODAY%.log
 echo ***********************************************************
 
 echo ***********************************************************
   echo ZLBTEST LBBOOKINGS Process Started At %TIME:~0,2%:%TIME:~3,2% >> HUBSPOT_API_%TODAY%.log
-  HubSpot_API.exe ZLBTEST BOOKINGS DEBUG >> HUBSPOT_API_%TODAY%.log
+  HubSpot_API.exe ZLBTEST BOOKINGS >> HUBSPOT_API_%TODAY%.log
 echo ***********************************************************
 
 pause
