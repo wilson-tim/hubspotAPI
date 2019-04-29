1a9c8d0c622d8b42726adc45f289a9c94a762da4
Tim Wilson <tim.wilson@travelopia.com>
2019-04-29 15:18:25 +0100

    Update Export_TO_CSV.ps1 to export one error message per line

--- batch.HubSpotAPI/Export_TO_CSV.ps1
+++ batch.HubSpotAPI/Export_TO_CSV.ps1
@@ -3,7 +3,7 @@ import-module "sqlps" -DisableNameChecking
 $currdate = $args[0]
 $expLocation = $args[1]
 
-Invoke-Sqlcmd -ServerInstance V-AWD-SQL-09 -querytimeout 0 -Query "SELECT * FROM [HUBSPOT].[dbo].[ST_ERROR_LOG] WITH (NOLOCK) WHERE Status='Fail' ORDER BY LogDate;" | Export-Csv -NoTypeInformation $($expLocation + "HUBSPOT_API_Errors_" + $currdate + ".csv")
+Invoke-Sqlcmd -ServerInstance V-AWD-SQL-09 -querytimeout 0 -Query "SELECT Brand,LifeCycleStage,ClientCode,VID,Email,IsNew,Status,REPLACE(Error_Msg,CHAR(10),CHAR(32)),LogDate FROM [HUBSPOT].[dbo].[ST_ERROR_LOG] WITH (NOLOCK) WHERE Status='Fail' ORDER BY LogDate;" | Export-Csv -NoTypeInformation $($expLocation + "HUBSPOT_API_Errors_" + $currdate + ".csv")
 Invoke-Sqlcmd -ServerInstance V-AWD-SQL-09 -querytimeout 0 -Query "SELECT * FROM [HUBSPOT].[dbo].[ST_ERROR_LOG] WITH (NOLOCK) WHERE Status='Success' ORDER BY LogDate;" | Export-Csv -NoTypeInformation $($expLocation + "HUBSPOT_API_Success_" + $currdate + ".csv")
 
 Invoke-SQLcmd -ServerInstance V-AWD-SQL-09 -querytimeout 0 -Query "exec [HUBSPOT].[dbo].[BUILD_HUBSPOT_TRITON_SS_BOOKINGS]" | Export-Csv -NoTypeInformation -Encoding utf8 $($expLocation + "HUBSPOT_TRITON_SS_BOOKINGS_" + $currdate + ".csv")
